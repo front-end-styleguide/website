0e322935ce9ca085d5f3bd4a3e1f7e7b43df485f
Fynn Becker <info@fynn-becker.de>
2017-08-31 17:38:39 +0200

    Clean up and preparations for documentation

---
+++ src/code/index/installation.sh
@@ -0,0 +1,13 @@
+# Get CLI access with Yarn
+yarn global add front-end-styleguide-cli
+# or with npm
+npm install -g front-end-styleguide-cli
+
+# Create a project folder
+mkdir new-project
+
+# Jump into the project folder
+cd new-project
+
+# Initialize a new project
+front-end-styleguide init
---
+++ src/code/index/usage.sh
@@ -0,0 +1,8 @@
+# Start development server
+front-end-styleguide dev
+
+# Run code linting
+front-end-styleguide test
+
+# Build production files
+front-end-styleguide build
